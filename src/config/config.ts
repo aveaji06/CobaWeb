b61d1f2eaeeafd177fdd72de6e6c801a8ec3dfd8
AveAji <aveaji987@gmail.com>
2025-05-11 16:38:38 +0700

    Perubahan 2

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,15 +4,15 @@ import { getDatabase } from "firebase/database";
 
 
   const DB_CONFIG ={
-    apiKey: "AIzaSyABTKRckyOfBqbHyNjLJWSCVZfjrEGhUIY",
-    authDomain: "coba-ddcac.firebaseapp.com",
-    databaseURL: "https://coba-ddcac-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "coba-ddcac",
-    storageBucket: "coba-ddcac.firebasestorage.app",
-    messagingSenderId: "915400809594",
-    appId: "1:915400809594:web:47827020a2430945172fc3",
-    measurementId: "G-JFN614W7V1"
-  };
+  apiKey: "AIzaSyAXAIs7ErQT5ZxWfs3-gufvzCknj2AAk24",
+  authDomain: "coba2-876b1.firebaseapp.com",
+  databaseURL: "https://coba2-876b1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "coba2-876b1",
+  storageBucket: "coba2-876b1.firebasestorage.app",
+  messagingSenderId: "873819054942",
+  appId: "1:873819054942:web:58cb418daf0ef297b2e5cf",
+  measurementId: "G-3GGND3QPB8"
+};
   const app = initializeApp(DB_CONFIG);
   const database = getDatabase(app);
   
--- src/pages/Dashboards/Historical/EmailData.tsx
+++ src/pages/Dashboards/Historical/EmailData.tsx
@@ -7,35 +7,35 @@ import { Link } from 'react-router-dom';
 const EmailData = () => {
     return (
         <React.Fragment>
-            <div className="col-span-6 card 2xl:col-span-6">
+            <div className="col-span-12 card 2xl:col-span-15 2xl:row-span-2">
                 <div className="card-body">
                     <div className="flex items-center gap-2">
                     </div>
                     <EnvironmentComparisonChartSuhu chartId="environmentDataChartSuhu" />
                 </div>
             </div>
-            <div className="col-span-6 card 2xl:col-span-6">
+            <div className="col-span-12 card 2xl:col-span-15 2xl:row-span-2">
                 <div className="card-body">
                     <div className="flex items-center gap-2">
                     </div>
                     <EnvironmentComparisonChartKelembaban chartId="environmentDataChartKelembaban" />
                 </div>
             </div>
-            <div className="col-span-6 card 2xl:col-span-6">
+            <div className="col-span-12 card 2xl:col-span-15 2xl:row-span-2">
                 <div className="card-body">
                     <div className="flex items-center gap-2">
                     </div>
                     <EnvironmentComparisonChartNH3 chartId="environmentDataChartNH3" />
                 </div>
             </div>
-            <div className="col-span-6 card 2xl:col-span-6">
+            <div className="col-span-12 card 2xl:col-span-15 2xl:row-span-2">
                 <div className="card-body">
                     <div className="flex items-center gap-2">
                     </div>
                     <EnvironmentComparisonChartCO2 chartId="environmentDataChartCO2" />
                 </div>
             </div>
-            <div className="col-span-6 card 2xl:col-span-6">
+            <div className="col-span-12 card 2xl:col-span-15 2xl:row-span-2">
                 <div className="card-body">
                     <div className="flex items-center gap-2">
                     </div>
